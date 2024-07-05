aa5529f4bd542195fc7184c4688e19f09f9fa1b2
James Alan Bush <jamesalanbush1972@gmail.com>
2024-07-05 14:37:44 -0400

    Initial Commit

---
+++ FileDocumentChatGPTTemplate/FileDocumentChatGPTTemplateApp.swift
@@ -0,0 +1,17 @@
+//
+//  FileDocumentChatGPTTemplateApp.swift
+//  FileDocumentChatGPTTemplate
+//
+//  Created by Xcode Developer on 7/5/24.
+//
+
+import SwiftUI
+
+@main
+struct FileDocumentChatGPTTemplateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
